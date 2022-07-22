c432736e744ef9993c38a606c594b06e7465e8e5
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-07-22 11:33:03 -0500

    lots of cleanup done, need to keep working

--- src/main/java/com/jonfriend/java47examprepcodecleanup/controllers/HomeController.java
+++ src/main/java/com/jonfriend/java47examprepcodecleanup/controllers/HomeController.java
@@ -20,10 +20,7 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import java.text.SimpleDateFormat; 
-import java.util.ArrayList; 
-
-//import Item.java; 
-import com.jonfriend.java47examprepcodecleanup.models.Item;
+ 
 import com.jonfriend.java47examprepcodecleanup.models.LoginUserMdl;
 import com.jonfriend.java47examprepcodecleanup.models.UserMdl;
 import com.jonfriend.java47examprepcodecleanup.services.UserSrv;
@@ -31,7 +28,6 @@ import com.jonfriend.java47examprepcodecleanup.services.UserSrv;
 @Controller
 public class HomeController {
 
-	// Add once service is implemented:
 	@Autowired
 	private UserSrv userSrv;
 	
@@ -39,67 +35,32 @@ public class HomeController {
 	public String index(
 			Model model
 			, HttpSession session) {
-		
-//		String fruitx = "apple"; 
-//		
-//		model.addAttribute("fruity", fruitx); 
-//		return "index.jsp"; 
-		
-		String customerName = "Buzz Aldrin"; 
-		String itemName = "Rolled Flag"; 
-		double itemPrice = 100000.00; 
-		String itemDescription = "Symbol of freedom and prosperity"; 
-		String vendorName = "Northrup Grummon"; 
-		
-		
-		model.addAttribute("customerName", customerName); 
-		model.addAttribute("itemName", itemName); 
-		model.addAttribute("itemPrice", itemPrice); 
-		model.addAttribute("itemDescription", itemDescription); 
-		model.addAttribute("vendorName", vendorName); 
-		
-		ArrayList<Item> fruitsArrList = new ArrayList<Item>();
-		fruitsArrList.add(new Item("Kiwi", 1.5));
-		fruitsArrList.add(new Item("Mango", 2.0));
-		fruitsArrList.add(new Item("Goji Berries", 4.0));
-		fruitsArrList.add(new Item("Guava", .75));
-		
-        model.addAttribute("fruitsFromHomeController", fruitsArrList);
         
-//        session.setAttribute("count", 0);
-        Integer currentCount = 0; 
-        if (session.getAttribute("count") == null) {
-    		// Use setAttribute to initialize the count in session
-        	session.setAttribute("count", 1);
-    	} else {
-    		// increment the count by 1 using getAttribute and setAttribute
-//    		session.count = session.getAttribute("count") += 1; 
-    		currentCount = (Integer) session.getAttribute("count");
-    		currentCount ++; 
-    		session.setAttribute("count", currentCount); 
-    		}
-         
-        model.addAttribute("countToShow", currentCount);
-        // GELD stuff
-        
-        if (session.getAttribute("currentCountGeld") == null) {
-        	session.setAttribute("currentCountGeld", 0);
-        }
-        
-//        if (session.getAttribute("geldActivityArrayList") == null) {
-//        	session.setAttribute("geldActivityArrayList", 0);
-//        }
-        
-        Integer currentCountGeld = (Integer) session.getAttribute("currentCountGeld");
-        model.addAttribute("currentCountGeld", currentCountGeld);
-        
-        
-		// If userId **is** found in session, redirect to home. 
-		if(session.getAttribute("userId") != null) {return "redirect:/home";}
+// ********************************************************************
+// AUTHENTICATION CONTROLS
+// ********************************************************************
 		
-		// login/reg form items: putting a new empty model (actually two mod) on the index page, so user can shove data onto using the form. 
+		// *** REDIRECT AUTH USERS TO /home METHOD -- DON'T EXPOSE REG/LOGIN index page TO ALREADY AUTH'ED USERS ***
+//		if(session.getAttribute("userId") != null) {return "redirect:/home";}
+
+		// login/reg form items: putting a new empty model for reg (and one for login too) on the index page, so user can shove data onto using the form. 
         model.addAttribute("newUser", new UserMdl());
         model.addAttribute("newLogin", new LoginUserMdl());
+        
+//      Begin: counter stuff
+        Integer currentCount = 0; 
+        // if session count hasn't started yet, i.e. first visit, set it to '1'
+        if (session.getAttribute("count") == null) {
+        	session.setAttribute("count", 1);
+        // if session already running, get the current count and add '1' to it.
+        } else { 
+        	currentCount = (Integer) session.getAttribute("count");
+        	currentCount ++; 
+        	session.setAttribute("count", currentCount); 
+        }
+        model.addAttribute("countToShow", currentCount);
+//        End: counter stuff
+        
 		return "index.jsp"; 
 	}
 
@@ -109,7 +70,6 @@ public class HomeController {
     		, BindingResult result
     		, Model model
     		, HttpSession session
-//    		, @ModelAttribute("newLogin") LoginUserMdl newLogin // added with JLee
     		) {
         
     	UserMdl user = userSrv.register(newUser, result);
@@ -118,18 +78,21 @@ public class HomeController {
         // to do some extra validations and create a new user!
         
         if(result.hasErrors()) {
-            // Be sure to send in the empty LoginUser before re-rendering the page.
+            // Below sends in the empty LoginUser before re-rendering the reg/login page; the reg model will maintain the incoming values to this method
             model.addAttribute("newLogin", new LoginUserMdl());
             return "index.jsp";
         }
         
-        // no errors?  here we go... 
-        //Store their ID from the DB in session, in other words, log them in.
+        // No errors?  Store their ID from the DB in session, log in.
         session.setAttribute("userId", user.getId());
    	 
-//	    return "redirect:/home";
-        // above is our dashboard; below is whatever "home app" you like to be skipping to.
-	    return "redirect:/store";
+	    return "redirect:/home";
+        // Above is our default dashboard location, which can be embraced if/when the app shall be expanded to have a "dashboard"/"home page".
+	    // This "home/dashboard" page can display content from numerous sub apps, as well as links to those apps.  
+	    // This general landing page, in other words, helps the app be readily scalable.  
+	    // At present, the /home route that we are directing to above will itself redirect to the presently-targeted app, 
+	    // thereby side-stepping the multi-faceted dashboard/home page.
+	    // When we are ready to have a mult-faceted webapp, just change the redirect on the /home method to instead render the home.jsp page. 
     }
     
     @PostMapping("/login")
@@ -138,9 +101,6 @@ public class HomeController {
     		, BindingResult result
     		, Model model
     		, HttpSession session) {
-        
-        // Add once service is implemented:
-        // User user = userServ.login(newLogin, result);
     	
     	UserMdl user = userSrv.login(newLogin, result);
     	
@@ -149,16 +109,10 @@ public class HomeController {
             return "index.jsp";
         }
     
-        // No errors! 
-        // TO-DO Later: Store their ID from the DB in session, 
-        // in other words, log them in.
-    
         session.setAttribute("userId", user.getId());
    	 
-//	    return "redirect:/home";
-        // above is our dashboard; below is whatever "home app" you like to be skipping to.
-	    return "redirect:/store";
-        
+	    return "redirect:/home";
+        // see comment in /register method regarding above redirect. Same thing applies.   
     }
     
     @GetMapping("/home")
@@ -166,108 +120,43 @@ public class HomeController {
 			Model model
 			, HttpSession session) {
 	 
-		// If no userId is found in session, redirect to logout.  JRF: put this on basically all methods now, except the login/reg pages
+		// Redirect all non-auth users to /logout method.
 		if(session.getAttribute("userId") == null) {return "redirect:/logout";}
 		
-		// We get the userId from our session (we need to cast the result to a Long as the 'session.getAttribute("userId")' returns an object
+		// create userId object from session.userid value.  We then use this object to display on page, and use for various validations. 
 		Long userId = (Long) session.getAttribute("userId");
 		model.addAttribute("user", userSrv.findById(userId));
 		
-//	    return "home.jsp";
+	    return "home.jsp";
 		// above is our dashboard; below is whatever "home app" you like to be skipping to.
-	    return "redirect:/store";
+//	    return "redirect:/store";
 	    
 	}
     
     @GetMapping("/logout")
 	public String logout(HttpSession session) {
-	 
-		// Set userId to null and redirect to login/register page
 		session.setAttribute("userId", null);
-	     
 	    return "redirect:/";
 	}
 
-//	@RequestMapping(value = "/processLogin", method=RequestMethod.POST)
-//	public String processLogin(
-//		@RequestParam(value="elNumero") String elNumero,
-//	    @RequestParam(value="city") String city,
-//	    @RequestParam(value="person") String person,
-//	    @RequestParam(value="profHobb") String profHobb,
-//	    @RequestParam(value="animal") String animal,
-//	    @RequestParam(value="nicety") String nicety,
-//		HttpSession session
-//		, RedirectAttributes redirectAttributes) 
-//		{
-//	    	// CODE TO PROCESS FORM ie. check email and password
-////			String emailSubmitted = email;
-//			
-//			session.setAttribute("elNumero", elNumero) ;
-//			session.setAttribute("city", city) ;
-//			session.setAttribute("person", person) ;
-//			session.setAttribute("profHobb", profHobb) ;
-//			session.setAttribute("animal", animal) ;
-//			session.setAttribute("nicety", nicety) ;
-//			
-//			redirectAttributes.addFlashAttribute("displayMessage", "Thanks for the good submission!");
-//	    	return "redirect:/dashboard"; // <-- we'll change this when we learn redirecting
-//	}
-//	
-//	@RequestMapping("/dashboard") 
-//	public String displayDashboard(Model model, HttpSession session) {
-//		// get any info needed out of session and add to the view model to render on the page.
-//        
-////    	String elNumero = (String) session.getAttribute("elNumero");
-////		model.addAttribute("elNumero", elNumero);
-////		
-////		String city = (String) session.getAttribute("city");
-////		model.addAttribute("city", city);
-////		
-////		String person = (String) session.getAttribute("person");
-////		model.addAttribute("person", person);
-//		return "dashboard.jsp"; 
-//	}
-	
+ // ********************************************************************
+ // MISCELLANEOUS CONTROLS
+ // ********************************************************************
+    
 	@RequestMapping("/reset-counter/")
 	public String resetCounter(HttpSession session, Model model, HttpServletRequest request) {
 		
 		session.setAttribute("count", 0);
-
 		return "redirect:/";
 	}
-	
-//	@RequestMapping("/counter")
-//	public String counterApp(Model model, HttpSession session, HttpServletRequest request) {
-//		Integer currentCount = 0; 
-//		if (session.getAttribute("count") == null) {
-//    		// Use setAttribute to initialize the count in session
-//        	session.setAttribute("count", 0);
-//    	} else {
-//    		// increment the count by 1 using getAttribute and setAttribute
-////    		session.count = session.getAttribute("count") += 1; 
-//    		currentCount = (Integer) session.getAttribute("count");
-//    	}
-//		
-//		// we can use the following line to get the URL of our page, in this case http://localhost:8080/counter/
-////		String homeUrl = request.getRequestURL().toString();
-//		
-//		model.addAttribute("countToShow", currentCount);
-//		
-////		model.addAttribute("page", homeUrl.substring(0,homeUrl.length()-9)); 
-//		
-//		return "counter.jsp"; 
-//	}
 	
 	@RequestMapping("/date")
 	public String dateRoute(Model model) {
 		
-
-		// JRF: below my 1st attempt
 		LocalDateTime myDateObj = LocalDateTime.now(); // Create a date object
 		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("E, dd MMM yyyy");
 		String formattedDate = myDateObj.format(myFormatObj);
 		model.addAttribute("displayDate", formattedDate);
-		// JRF end
 		
 		SimpleDateFormat dayName = new SimpleDateFormat("EEEE");
 		SimpleDateFormat dayOfMonth = new SimpleDateFormat("dd");
@@ -284,7 +173,6 @@ public class HomeController {
 		String comprehensiveDateString = dayNameString + ", the " + dayOfMonthString + " of "  + spelledOutMonthString + ", " + yearFourNumsString + " "; 
 		
 		model.addAttribute("displayDate2", comprehensiveDateString);
-		
 		
 		return "date.jsp"; 
 	}
@@ -304,57 +192,6 @@ public class HomeController {
 		
 		return "time.jsp"; 
 	}
-	
-	@RequestMapping("/dojos")
-    public String dojosRoute(Model model) {
-        ArrayList<String> dojosArrList = new ArrayList<String>();
-        dojosArrList.add("Burbank");
-        dojosArrList.add("Chicago");
-        dojosArrList.add("Bellevue");
-        model.addAttribute("dojosFromMyController", dojosArrList);
-        return "dojos.jsp";
-    }
-	
-	//	@RequestMapping("")
-//	public String hello() {
-//		return "19 Welcome!"; 
-//	}
-	
-//	@RequestMapping("/today")
-//	public String today() {
-//		return "19 Today you will find luck in all your endeavors!"; 
-//	}
-//	
-//	@RequestMapping("/tomorrow")
-//	public String tommy() {
-//		return "19 Tomorrow, an opportunity will arise, so be sure to be open to new ideas!"; 
-//	}
-//	
-//	@RequestMapping("/travel")
-//	public String travvy0() {
-//		return "19 You want to get away, I get it."; 
-//	}
-//	
-//	@RequestMapping("/travel/{destination}")
-//	public String travvy(@PathVariable("destination") String destination) {
-//		return "19 Congrats!  You will soon travel to " + destination + "!" ; 
-//	}
-//	
-//	@RequestMapping("/lotto")
-//	public String lotty0() {
-//		return "19 get some advice here, dude."; 
-//	}
-//	
-//	@RequestMapping("/lotto/{intyInt}")
-//	public String lotty(@PathVariable("intyInt") Integer intyInt) {
-//		if (intyInt % 2 == 0) {
-//			return "You will take a grand journey in the near future, but be weary of tempting offers."; 
-//		} else {
-//			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends."; 
-//		}
-//		
-//		 
-//	}
 
 // end of methods
 }
--- src/main/webapp/WEB-INF/include/header.jsp
+++ src/main/webapp/WEB-INF/include/header.jsp
@@ -8,16 +8,15 @@
 			<a href="/logout">Logout</a>
 		</div>
 		<div id=nav class="container-fluid">
-			<!-- <a href="home">Home</a> 
+			<a href="/home">Home</a> 
 			<a href= "/date">Date Template</a>
 			<a href= "/time">Time Template</a>
-			<a href= "/dojos">Dojos</a>
 			<a href= "/books">Books</a>
 			<a href= "/expenses">Expenses</a>
 			<a href= "/expensesAndCreate">expensesAndCreate</a>
 			<a href="/dojo">Dojo List</a>
 			<a href= "/ninja">Ninja List</a>
-			<a href= "/publication">Book Club</a> -->
+			<a href= "/publication">Book Club</a>
 			<a href= "/store">Store</a>
 		</div>
 	</div>
